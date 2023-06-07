a9baac67adede02763cbf5b261ef0686fd5b1b73
Nora <nora.galina@sk.lvt.lv>
2023-06-07 08:19:57 +0300

    Uztaisita iespeja mainit masinu lielumu un neielikt tas savas vietas

--- Assets/Skripti/NomesanasVieta.cs
+++ Assets/Skripti/NomesanasVieta.cs
@@ -100,6 +100,43 @@ public class NomesanasVieta : MonoBehaviour, IDropHandler {
                     case "buss":
                         objektuSkripts.autobuss.GetComponent<RectTransform>().localPosition = objektuSkripts.bussKoord;
                         break;
+
+                    case "b2":
+                        objektuSkripts.b2.GetComponent<RectTransform>().localPosition = objektuSkripts.b2Koord;
+                        break;
+
+                    case "cement":
+                        objektuSkripts.cementaMasina.GetComponent<RectTransform>().localPosition = objektuSkripts.cemMKoord;
+                        break;
+
+                    case "e46":
+                        objektuSkripts.e46.GetComponent<RectTransform>().localPosition = objektuSkripts.e46Koord;
+                        break;
+
+                    case "e61":
+                        objektuSkripts.e61.GetComponent<RectTransform>().localPosition = objektuSkripts.e61Koord;
+                        break;
+
+                    case "esk":
+                        objektuSkripts.eskavators.GetComponent<RectTransform>().localPosition = objektuSkripts.eskKoord;
+                        break;
+
+                    case "police":
+                        objektuSkripts.policija.GetComponent<RectTransform>().localPosition = objektuSkripts.polKoord;
+                        break;
+
+                    case "trak1":
+                        objektuSkripts.traktors1.GetComponent<RectTransform>().localPosition = objektuSkripts.trak1Koord;
+                        break;
+
+                    case "trak5":
+                        objektuSkripts.traktors2.GetComponent<RectTransform>().localPosition = objektuSkripts.trak2Koord;
+                        break;
+
+                    case "uguns":
+                        objektuSkripts.ugunsdzeseji.GetComponent<RectTransform>().localPosition = objektuSkripts.ugunsKoord;
+                        break;
+
                 }
 
             }
---
+++ Assets/Skripti/ObjektuTransformacija.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjektuTransformacija : MonoBehaviour {
+
+    public Objekti objektuSkripts;
+
+    void Update()
+    {
+        if (objektuSkripts.pedejaisVilktais != null)
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0, 0, Time.deltaTime * 10f);
+            }
+            if (Input.GetKey(KeyCode.X))
+            {
+                objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.Rotate(0, 0, -Time.deltaTime * 10f);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y <= 0.85f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().
+                        localScale.x, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y + 0.001f);
+                }
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y >= 0.3f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().
+                        localScale.x, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y - 0.001f);
+                }
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x >= 0.3f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().
+                        localScale.x - 0.001f, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y);
+                }
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.x <= 0.85f)
+                {
+                    objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().
+                        localScale.x + 0.001f, objektuSkripts.pedejaisVilktais.GetComponent<RectTransform>().localScale.y);
+                }
+            }
+        }
+    }
+}
