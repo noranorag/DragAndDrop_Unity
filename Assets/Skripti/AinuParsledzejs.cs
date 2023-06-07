9b6fa6bb3d255fa45c6aeee9d053295f4e36edee
Nora <nora.galina@sk.lvt.lv>
2023-06-07 08:34:29 +0300

    Uztaisits base sakuma ekranam

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejs : MonoBehaviour {
+
+    //Ielade sakuma ekranu
+    
+    public void uzSakumu()
+    {
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+
+    //Ielade pašu spēli
+
+    public void uzPilsetasAina()
+    {
+        SceneManager.LoadScene("PilsetasAina", LoadSceneMode.Single);
+    }
+
+    //Aizver programmu
+
+    public void Apturet()
+    {
+        Application.Quit();
+    }
+
+}
